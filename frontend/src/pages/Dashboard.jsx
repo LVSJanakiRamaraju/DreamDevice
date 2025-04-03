b94aa02cd58628d3d6b6d8f0597284de0a98b7c7
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-04-03 20:02:28 +0530

    Created frontend folder and Install required dependencies

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+    return (
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Dashboard</h1>
+      </div>
+    );
+  }
+  
+  export default Dashboard;
+  
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="h-screen flex justify-center items-center text-2xl">
+        Welcome to Dream Device Marketplace
+      </div>
+    );
+  }
+  
+  export default Home;
+  
