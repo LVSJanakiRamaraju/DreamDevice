11f6f79f390e2ced85a7cc8dbb2dcea1874815db
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-04-04 19:32:37 +0530

    Added login and Register in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
-function Home() {
-    return (
-      <div className="h-screen flex justify-center items-center text-2xl">
-        Welcome to Dream Device Marketplace
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Dream Device</h1>
+      <p>Select an option below to continue:</p>
+      <Link to="/login">
+        <button style={{ margin: '10px' }}>Login</button>
+      </Link>
+      <Link to="/register">
+        <button style={{ margin: '10px' }}>Register</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
